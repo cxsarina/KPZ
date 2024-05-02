edae21c8ddc9f62ce69dd474e85ae362ad8aecf5
Aryna Shkolna <cxsarina@gmail.com>
2024-05-02 13:46:47 +0300

    Додавання хуків життєвого циклу до базового класу LightNode

--- lab-5/lab-5/ClassLibrary/LightNode.cs
+++ lab-5/lab-5/ClassLibrary/LightNode.cs
@@ -10,5 +10,34 @@
         {
             Click?.Invoke(this, message);
         }
+        public virtual void OnCreated()
+        {
+            Console.WriteLine($"Node created: {GetType().Name}");
+        }
+
+        public virtual void OnInserted()
+        {
+            Console.WriteLine($"Node inserted: {GetType().Name}");
+        }
+
+        public virtual void OnRemoved()
+        {
+            Console.WriteLine($"Node removed: {GetType().Name}");
+        }
+
+        public virtual void OnStylesApplied()
+        {
+            Console.WriteLine($"Styles applied to node: {GetType().Name}");
+        }
+
+        public virtual void OnClassListApplied()
+        {
+            Console.WriteLine($"Classes applied to node: {GetType().Name}");
+        }
+
+        public virtual void OnTextRendered()
+        {
+            Console.WriteLine($"Text rendered for node: {GetType().Name}");
+        }
     }
 }
